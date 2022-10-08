12b9493a4f7ebb56b74bb1208a082acdcb5aab09
JunyaLab <junjunnacky1@gmail.com>
2022-10-08 23:32:50 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// const a = document.getElementById('test');
+
+// a.addEventListener('mouseover', function() {
+//     a.style.backgroundColor = 'rgba(0,0,0,.3)';
+// }, false);
+
+// a.addEventListener('mouseleave', function() {
+//     a.style.backgroundColor = '#628199';
+// }, false);
+
+// function sample(a) {
+//     a.addEventListener('mouseover', function() {
+//         a.style.backgroundColor = 'rgba(0,0,0,.3)';
+//     }, false);
+    
+//     a.addEventListener('mouseleave', function() {
+//         a.style.backgroundColor = '#628199';
+//     }, false);
+// }
